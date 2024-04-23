9be8d9936508b652ce948c9db6e602779496179b
ByronRobotics5641 <166260278+ByronRobotics5641@users.noreply.github.com>
2024-04-23 18:32:29 -0400

    Added LimeLight Subsystem

---
+++ src/main/java/frc/robot/robot/subsystems/LimeLightSubsystem.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LimeLightSubsystem extends SubsystemBase {
+  /** Creates a new LimeLightSubsystem. */
+  public LimeLightSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
